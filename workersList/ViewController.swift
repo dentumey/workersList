1c1ca13299da32aca7dda572c02cfa4cc7655306
Den <dentumey@gmail.com>
2022-11-16 21:33:34 +0300

    Initial Commit

---
+++ workersList/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  workersList
+//
+//  Created by Dentumei Lutfullin on 16.11.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
